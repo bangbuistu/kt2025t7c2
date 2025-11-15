8ef3d7fad8b6f9e956d4aac565fbfc772fc7011c
hime905 <trunghoa6141@gmail.com>
2025-11-15 10:55:26 +0700

    cau 2 cpp

---
+++ cau2.cpp
@@ -0,0 +1,17 @@
+#include <isotream>
+
+void main(){
+ void kiemtrasongto(a){
+	if(a <= 2)
+	cout<< " day khong phai so nguyento"
+	if (a=2)
+	return " Day la so nguyen to chan "
+ 	for ( int i=2; i< a;i++){
+	if(a%i==0)
+		cout<<" day khong phai la so nguyen to"
+ 	}
+	if(a%2==0)
+		cout<<"day la so ngto chan "
+	else
+		cout<<"day la so ngto le"
+}
