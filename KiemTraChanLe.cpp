6765ad39f162d6af3ba2ecb280202b9f072479ac
LeGiaThien <lp.a7.0404@gmail.com>
2025-11-15 10:39:15 +0700

    KTChanLe

---
+++ KiemTraChanLe.cpp
@@ -0,0 +1,13 @@
+#include<iostream>;
+using namespace std;
+int void main(){
+int a;
+cout<<"Nhap vao so nguyen can kiem tra"<<endl;
+cin>>a;
+if(a%2==0){
+cout<<"La so chan";
+}
+else {
+cout<<"La so le";
+}
+}
