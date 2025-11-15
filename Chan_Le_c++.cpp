c9e093e85bfe93d3a8d649eefd32eeddab6119a4
plastma65 <plastma65@gmail.com>
2025-11-15 10:49:40 +0700

    Chan_Le_C++

---
+++ Chan_Le_c++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#using namespace std;
+
+int main(){
+	int a;
+	cout << "Nhap so nguyen a"
+	cin >> a;
+	if(a > 0) {
+		cout << "so chan!"
+	} else {
+		cout << "so le!"
+	}
+}
