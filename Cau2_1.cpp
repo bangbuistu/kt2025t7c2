02f82dafcf4e1e548d5a33d7f6ba48f3c6b376f4
minh <trinhnhatmiki@gmail.com>
2025-11-15 10:35:19 +0700

    Them file cpp

---
+++ Cau2_1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n%3==0) cout<<"Day la boi so cua 3";
+    else cout<<"Day khong phai la boi so cua 3";
+    return 0;
+}
