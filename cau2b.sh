b4936f4a32318d35a2d34720e93c18580828c31d
phanvykhang <raichu1253@gmail.com>
2025-11-15 10:47:06 +0700

    suaketqua

--- cau2b.sh
+++ cau2b.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 $so=2
-if [[ $so%2 ==0]]; then
+if [[ $so%2 eq 0]]; then
 	echo "Day la so chan"
-elif [[ $so%2!=0 ]]; then
+elif [[ $so%2 eq 0 ]]; then
 	echo "Day la so le"
 fi
